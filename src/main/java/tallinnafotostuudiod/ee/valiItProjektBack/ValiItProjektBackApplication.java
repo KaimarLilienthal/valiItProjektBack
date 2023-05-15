3862fa983fd1835f507ebf428cd68d81ba0f45d1
Kaimar <kaimarlilienthal@gmail.com>
2023-05-15 08:40:42 +0300

    Initial commit

---
+++ src/main/java/tallinnafotostuudiod/ee/valiItProjektBack/ValiItProjektBackApplication.java
@@ -0,0 +1,13 @@
+package tallinnafotostuudiod.ee.valiItProjektBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValiItProjektBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValiItProjektBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tallinnafotostuudiod/ee/valiItProjektBack/ValiItProjektBackApplicationTests.java
@@ -0,0 +1,13 @@
+package tallinnafotostuudiod.ee.valiItProjektBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValiItProjektBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
